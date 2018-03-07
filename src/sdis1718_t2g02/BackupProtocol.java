a8aadf781b6c6f7e2d4863527a6ee5b1b1788201
danrpinho <up201505302@fe.up.pt>
2018-03-07 12:53:06 +0000

    Working on file and header management.

--- src/sdis1718_t2g02/BackupProtocol.java
+++ src/sdis1718_t2g02/BackupProtocol.java
@@ -1,18 +1,18 @@
 	package sdis1718_t2g02;
 
 public class BackupProtocol extends Protocol {
-
-	public BackupProtocol() {
-		// TODO Auto-generated constructor stub
+		
+	public BackupProtocol(String version, String senderID) {
+		super(version, senderID);
 	}
 	
 	private byte[] createPutchunkHeader(String version, String senderID, String fileID, int chunkNo, int replicationDeg) {
-		byte[] res = super.createHeader(MessageType.PUTCHUNK, version, senderID, fileID, chunkNo, replicationDeg);
+		byte[] res = super.createHeader(MessageType.PUTCHUNK, fileID, chunkNo, replicationDeg);
 		return res;
 	}
 	
 	private byte[] createStoredHeader(String version, String senderID, String fileID, int chunkNo, int replicationDeg) {
-		byte[] res = super.createHeader(MessageType.PUTCHUNK, version, senderID, fileID, chunkNo, -1);
+		byte[] res = super.createHeader(MessageType.STORED, fileID, chunkNo, -1);
 		return res;
 	}
 
--- src/sdis1718_t2g02/Protocol.java
+++ src/sdis1718_t2g02/Protocol.java
@@ -1,12 +1,21 @@
 package sdis1718_t2g02;
 
-public class Protocol {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.util.Objects;
 
+public abstract class Protocol {
+	
+	private String version;
+	private String senderID;
+	
 	public enum MessageType {
 		PUTCHUNK, STORED, GETCHUNK, CHUNK, DELETE, REMOVED
 	};
 
-	public byte[] createHeader(MessageType type, String version, String senderID, String fileID, int chunkNo,
+	public byte[] createHeader(MessageType type, /*String version, String senderID,*/ String fileID, int chunkNo,
 			int replicationDeg) {		
 		switch (type) {
 		case PUTCHUNK: 
@@ -23,6 +32,22 @@ public class Protocol {
 		default:
 		}
 		return null;
+	}
+	
+	public String getFileData(File file) throws IOException {
+		String name = file.getName();
+		String size = Objects.toString(file.length());
+		String last = Files.getLastModifiedTime(file.toPath(), LinkOption.NOFOLLOW_LINKS).toString();
+		String owner = Files.getOwner(file.toPath(), LinkOption.NOFOLLOW_LINKS).toString();
+		String res =  name + "::" + size + "::" + last + "::" + owner;
+		return res;
+	}
+	
+	
+	
+	public Protocol(String version, String senderID) {
+		this.version = version;
+		this.senderID = senderID;
 	}
 
 }
