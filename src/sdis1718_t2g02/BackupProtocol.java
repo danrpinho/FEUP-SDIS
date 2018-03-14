ca602252540db45323bdef9a769d26f54c5186a3
danrpinho <up201505302@fe.up.pt>
2018-03-14 11:36:00 +0000

    Probably finished implementing Header function (finally)

--- src/sdis1718_t2g02/BackupProtocol.java
+++ src/sdis1718_t2g02/BackupProtocol.java
@@ -1,9 +1,12 @@
 	package sdis1718_t2g02;
 
 public class BackupProtocol extends Protocol {
-		
-	public BackupProtocol(String version, String senderID) {
+	
+	private int replicationDeg;
+	
+	public BackupProtocol(String version, String senderID, int replicationDeg) {
 		super(version, senderID);
+		this.replicationDeg = replicationDeg;
 	}
 	
 	private byte[] createPutchunkHeader(String version, String senderID, String fileID, int chunkNo, int replicationDeg) {
--- src/sdis1718_t2g02/Protocol.java
+++ src/sdis1718_t2g02/Protocol.java
@@ -7,44 +7,35 @@ import java.nio.file.LinkOption;
 import java.util.Objects;
 
 public abstract class Protocol {
-	
+
 	private String version;
 	private String senderID;
-	
+
 	public enum MessageType {
 		PUTCHUNK, STORED, GETCHUNK, CHUNK, DELETE, REMOVED
 	};
 
-	public byte[] createHeader(MessageType type, /*String version, String senderID,*/ String fileID, int chunkNo,
-			int replicationDeg) {		
-		switch (type) {
-		case PUTCHUNK: 
-			break;
-		case STORED:
-			break;
-		case GETCHUNK:
-			break;
-		case CHUNK:
-			break;
-		case DELETE:
-			break;
-		case REMOVED:
-		default:
-		}
-		return null;
+	public byte[] createHeader(MessageType type, String fileID, int chunkNo, int replicationDeg) {
+		String res = new String();
+		if (replicationDeg == -1) {
+			if (chunkNo == -1)
+				res = type.name() + " " + this.version + " " + this.senderID + " " + fileID;
+			else
+				res = type.name() + " " + this.version + " " + this.senderID + " " + fileID + " " + chunkNo;
+		} else
+			res = type.name() + " " + this.version + " " + this.senderID + " " + fileID + " " + chunkNo + " " + replicationDeg;
+		return res.getBytes();
 	}
-	
+
 	public String getFileData(File file) throws IOException {
 		String name = file.getName();
 		String size = Objects.toString(file.length());
 		String last = Files.getLastModifiedTime(file.toPath(), LinkOption.NOFOLLOW_LINKS).toString();
 		String owner = Files.getOwner(file.toPath(), LinkOption.NOFOLLOW_LINKS).toString();
-		String res =  name + "::" + size + "::" + last + "::" + owner;
+		String res = name + "::" + size + "::" + last + "::" + owner;
 		return res;
 	}
-	
-	
-	
+
 	public Protocol(String version, String senderID) {
 		this.version = version;
 		this.senderID = senderID;
